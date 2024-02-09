401a0089d0217852533881f8fa6424af6235cdd3
Shashank Rustagi <mathisfunlike321@gmail.com>
2024-02-10 01:28:16 +0530

    updated code for cards.tsx commenting out the totalPaidInvoices as they arent used as of now

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -16,7 +16,7 @@ const iconMap = {
 export default async function CardWrapper() {
   return (
     <>
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
